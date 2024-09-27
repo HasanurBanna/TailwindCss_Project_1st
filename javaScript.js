585d53dea1d95092735e5f434d956b6fcf588d84
HasanurBanna <hasanurbanna03@gmail.com>
2024-09-27 23:30:57 +0600

    full_project_uplod

---
+++ javaScript.js
@@ -0,0 +1,5 @@
+const htmlTag = document.getElementById("htmlTag");
+
+document.getElementById("sun").addEventListener("click", function () {
+    htmlTag.classList.toggle("dark");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Gowun': ['"Gowun Batang", serif;']
+      },
+    },
+  },
+  plugins: [],
+}
+
